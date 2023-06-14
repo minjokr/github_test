d02bb10597cafd246cad03e497f562707d9976b2
Min Jo <whals777530@naver.com>
2023-06-14 16:48:03 +0900

    06-14 4:47 wed

---
+++ Test.java
@@ -0,0 +1,11 @@
+package net.hb.day30;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이름: 고길동");
+		System.out.println("동네: 홍대입구");
+		System.out.println("나이: 27");
+	}
+} // class END 
